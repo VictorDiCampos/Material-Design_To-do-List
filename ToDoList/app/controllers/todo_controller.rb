b96ab7574b850ec1438db09c17a51b6c6d9f4c17
VictorDiCampos <victordicampos@gmail.com>
2016-05-02 19:49:52 -0300

    Adicionando estilos e rotas.

--- ToDoList/app/controllers/todo_controller.rb
+++ ToDoList/app/controllers/todo_controller.rb
@@ -1,4 +1,30 @@
 class TodoController < ApplicationController
   def index
   end
+
+  def index
+  end
+
+  def show
+  end
+
+  def new
+	end
+
+  def edit
+  end
+
+  def create
+  end
+
+  def update
+  end
+
+  def destroy
+  end 
+
+  private
+    def article_params
+      
+    end
 end
