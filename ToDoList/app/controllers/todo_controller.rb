f86f0f3c8cb71b663d0729f543291c9d5f58964f
VictorDiCampos <victordicampos@gmail.com>
2016-05-03 13:20:14 -0300

    Changing form elements.

--- ToDoList/app/controllers/todo_controller.rb
+++ ToDoList/app/controllers/todo_controller.rb
@@ -2,19 +2,22 @@ class TodoController < ApplicationController
   def index
   end
 
-  def index
-  end
-
   def show
   end
 
   def new
+    
 	end
 
   def edit
   end
 
   def create
+    #@restaurante = Restaurante.new params.require(:restaurante).permit(:nome, :endereco, :especialidade)
+    #@restaurante.save
+    # render plain: params[:article].inspect
+    
+    render plain: params[:todo].inspect
   end
 
   def update
