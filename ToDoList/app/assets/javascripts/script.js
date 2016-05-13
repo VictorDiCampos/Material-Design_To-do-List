521302473a8f7ac1679d5c9ef16ceb5bcaa76364
VictorDiCampos <victordicampos@gmail.com>
2016-05-12 21:48:14 -0300

    [angularJS] Finalizando C e R do CRUD.

--- ToDoList/app/assets/javascripts/script.js
+++ ToDoList/app/assets/javascripts/script.js
@@ -13,64 +13,21 @@ $(document).ready(function(){
   	$scope.delTask = function(id){
 			$http.post('/deltask',{id: id}, {headers:{ 'X-CSRF-Token':  $('meta[name=csrf-token]').attr('content') }}).success(function(data){
 				for (var i = 0; i < $scope.tasks.length; i++) {
-					
-					console.log("id-Task:", id);
-					console.log("id-atual:", $scope.tasks[i].id);
-
-					if($scope.tasks[i].id === id){
-						// $scope.tasks(data[i]);
-						$scope.oldTasks = $scope.tasks;
-						console.log("oldTasks: ", $scope.oldTasks);
-						$scope.tasks = $scope.tasks.slice(0, i);
-						console.log("slice: ", $scope.tasks);
-						$scope.oldTasks = $scope.oldTasks.slice(i+1);
-						console.log("resto: ", $scope.oldTasks);
-						$scope.tasks.concat($scope.oldTasks);
-						console.log("final: ", $scope.tasks);
+					if($scope.tasks[i].id === id){												
+						$scope.tasks = $scope.tasks.slice(0, i).concat($scope.oldTasks.slice(i+1));
 					}				
 				};
 			});  		
   	}
 
   	$scope.addTask = function(description){  		
-  		$http.post('/newtask',{description: description}, {headers:{ 'X-CSRF-Token':  $('meta[name=csrf-token]').attr('content') }}).success(function(data){
-  			// console.log(data);
-  			$scope.tasks.push(data);
+  		$http.post('/newtask',{description: description}, {headers:{ 'X-CSRF-Token':  $('meta[name=csrf-token]').attr('content') }}).success(function(data){  			
+  			$newtask = {id: data.todo.id, description: data.todo.description};
+  			$scope.tasks.push($newtask);  			
   		});
-
-  	// 	$input = $('.mdl-textfield__input');
-			// $label = $('.mdl-textfield__label');
-
-			// if($input.val().length > 0) {				
-
-			// 	$task = '<div class="task">'
-			// 	+'<label class="mdl-switch mdl-js-switch mdl-js-ripple-effect" for="switch-1">'
-			// 	+'<input type="checkbox" id="switch-1" class="mdl-switch__input">'
-			// 	+'<span class="mdl-switch__label">'+$(".mdl-textfield__input").val()+ '</span>'
-			// 	+'</label><span class="rm-task"><i class="material-icons">clear</i></div>';
-
-			// 	$task = '<div class="task" ng-repeat='task in tasks'>'
-   //      +'<label class="mdl-switch mdl-js-switch mdl-js-ripple-effect" for="switch-1">'
-   //      +'<input type="checkbox" id="switch-1" class="mdl-switch__input">'
-   //      +'<span class="mdl-switch__label" data-id={{task.id}}>{{task.description}}</span>'
-   //      +'</label>'
-   //      +'<span class="rm-task" ng-click="delTask(task.id)"><i class="material-icons">clear</i>'
-   //      +'</div>';
-				// $(".task-list").append($task);
-
-			// 	ids.push( data.todo.id );
-				
-			// }
-
-			// else if ( $input.val().length === 0 && $('.input-error').length === 0 ) {
-			// 	$label.css('color', 'red');		
-			// 	$message = '<span class="input-error">O campo a cima e obrigatorio.</span>';
-			// 	$(".add-task").append($message);			
-			// }
-  		
+	
   	}
 
 	});//fim taskListController
-
 	
 });//fim $(document).ready
